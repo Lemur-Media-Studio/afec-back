fa6ba8a6d53077b6af35a37df920c9c0d91e3be1
facumruiz <facundoruiz105@gmail.com>
2023-07-03 09:16:30 -0300

    first commit

---
+++ Back/models/usuariosModel.js
@@ -0,0 +1,32 @@
+const mongoose = require('../bin/mongodb')
+const bcrypt = require('bcrypt');
+const Schema = mongoose.Schema;
+
+const MainSchema = new Schema({
+    name:{
+        type: String,
+        trim: true,
+        max: 20 [true, "El nombre no debe superar los 20 caracteres"]
+    },
+    surname:{
+        type: String,
+        trim: true,
+        max: 20 [true, "El apellido no debe superar los 20 caracteres"]
+    },
+    user:{
+        type: String,
+        unique: true,
+        required: [true,"El campo usuario es obligatorio"],
+        max: 20 [true, "El apellido no debe superar los 20 caracteres"]
+    },
+    password:{
+        type: String,
+        required: true
+    }
+})
+MainSchema.pre('save',function(next){
+    console.log(this.password)
+    this.password = bcrypt.hashSync(this.password,10);
+    next();
+})
+module.exports = mongoose.model('usuarios',MainSchema)
