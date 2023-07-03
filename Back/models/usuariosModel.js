905225370fc983c5d731d7a8b5c5c6a8cf3505e5
facumruiz <facundoruiz105@gmail.com>
2023-07-03 09:39:26 -0300

    add validator schema user

--- Back/models/usuariosModel.js
+++ Back/models/usuariosModel.js
@@ -2,16 +2,42 @@ const mongoose = require('../bin/mongodb')
 const bcrypt = require('bcrypt');
 const Schema = mongoose.Schema;
 
+var nameValidator = [
+    validate({
+      validator: 'isLength',
+      arguments: [3, 20],
+      message: 'Name should be between 3 and 50 characters'
+    }),
+    validate({
+      validator: 'isAlphanumeric',
+      passIfEmpty: true,
+      message: 'Name should contain alpha-numeric characters only'
+    })
+  ];
+
+  var surnameValidator = [
+    validate({
+      validator: 'isLength',
+      arguments: [3, 20],
+      message: 'Name should be between 3 and 50 characters'
+    }),
+    validate({
+      validator: 'isAlphanumeric',
+      passIfEmpty: true,
+      message: 'Name should contain alpha-numeric characters only'
+    })
+  ];
+
 const MainSchema = new Schema({
     name:{
         type: String,
         trim: true,
-        maxLength: 20 [true, "El nombre no debe superar los 20 caracteres"]
+        validate:nameValidator
     },
     surname:{
         type: String,
         trim: true,
-        maxLength: 20 [true, "El apellido no debe superar los 20 caracteres"]
+        validate:surnameValidator
     },
     user:{
         type: String,
