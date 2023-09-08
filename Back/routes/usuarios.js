0ffce0307b6ed4ca95036ca1c059b8f54937350d
facumruiz <facundoruiz105@gmail.com>
2023-09-08 11:59:10 -0300

    fix1 usuariosController

--- Back/routes/usuarios.js
+++ Back/routes/usuarios.js
@@ -7,8 +7,7 @@ var mainController = require("../controllers/usuariosController")
 /* GET home page. */
 router.post('/registro', mainController.create);
 router.post('/login', mainController.login);
-router.post('/logout', mainController.logout);
-router.get('/profile', mainController.authRequired, mainController.profile)
+
 
 
 module.exports = router;
