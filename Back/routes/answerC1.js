3c8367fcff0be94927e92b0ed583e2c1b71bc661
facumruiz <facundoruiz105@gmail.com>
2023-09-08 18:06:22 -0300

    fix find collection c1

--- Back/routes/answerC1.js
+++ Back/routes/answerC1.js
@@ -8,7 +8,7 @@ var answerC1Controller = require("../controllers/answerC1Controller")
 
 //router.get('/', (req,res,next)=>{req.app.validateUser(req,res,next)},answerC1Controller.getAll);
 
-router.get('/respuestas/:id',(req,res,next)=>{req.app.validateUser(req,res,next)}, answerC1Controller.getById, answerC1Controller.getAll);
+router.get('/respuestas',answerC1Controller.getAll);
 router.post('/respuestas', answerC1Controller.create);
 
 
