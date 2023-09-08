ebe8777b80d0f674bb78ff95806911c82c2e287d
facumruiz <facundoruiz105@gmail.com>
2023-09-08 11:41:10 -0300

    test 1 secrectKey

--- Back/controllers/usuariosController.js
+++ Back/controllers/usuariosController.js
@@ -49,21 +49,19 @@ module.exports = {
     },
 
     authRequired: async function (req, res, next) {
-        const {token} = req.cookies
-        if (!token) return res.status(401).json({message: "no token"})
-
-        jwt.verify(token,req.headers['x-access-token'], req.app.get('secretKey'),function(err,decoded){
-            if(err){
-              res.json({message:err.message})
-            }else{
-              //console.log(decoded)
-              req.body = decoded
-            
-            }
-          })
-          next();
-        
-    },
+        console.log(req.app.get('secretKey'))
+        /*
+        jwt.verify(req.headers['x-access-token'],req.app.get('secretKey'),function(err,decoded){
+          if(err){
+            res.json({message:err.message})
+          }else{
+            console.log(decoded)
+            req.body.userToken = decoded
+            next();
+          }
+        })
+        */
+      },
 
     profile: async function (req, res, next) {
     
