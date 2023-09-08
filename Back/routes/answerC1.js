677a9a34639eb2269902b37ba0288b97670fe597
facumruiz <facundoruiz105@gmail.com>
2023-09-08 18:54:20 -0300

    add getById answerC1

--- Back/routes/answerC1.js
+++ Back/routes/answerC1.js
@@ -9,6 +9,7 @@ var answerC1Controller = require("../controllers/answerC1Controller")
 //router.get('/', (req,res,next)=>{req.app.validateUser(req,res,next)},answerC1Controller.getAll);
 
 router.get('/respuestas',answerC1Controller.getAll);
+router.get('/respuestas/:id',answerC1Controller.getById);
 router.post('/respuestas', answerC1Controller.create);
 
 
