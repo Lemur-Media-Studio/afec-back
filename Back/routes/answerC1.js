dd2f33c04f04812a1c725e70623c708d84042769
facumruiz <facundoruiz105@gmail.com>
2023-09-08 16:52:24 -0300

    add new schema

--- Back/routes/answerC1.js
+++ Back/routes/answerC1.js
@@ -7,7 +7,7 @@ var answerC1Controller = require("../controllers/answerC1Controller")
 //router.get('/', (req,res,next)=>{req.app.validateUser(req,res,next)},answerC1Controller.getAll);
 
 //router.get('/', (req,res,next)=>{req.app.validateUser(req,res,next)},answerC1Controller.getAll);
-router.get('/respuestas', answerC1Controller.getAll);
+
 router.get('/respuestas/:id',(req,res,next)=>{req.app.validateUser(req,res,next)}, answerC1Controller.getById);
 router.post('/respuestas', answerC1Controller.create);
 
