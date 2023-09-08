3e42a9e10bab04d214c775f42220ac203d72f0db
facumruiz <facundoruiz105@gmail.com>
2023-09-08 14:22:13 -0300

    return json value users

--- Back/controllers/usuariosController.js
+++ Back/controllers/usuariosController.js
@@ -3,38 +3,44 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 module.exports = {
-    
-    create: async function(req, res, next) {
+
+    create: async function (req, res, next) {
         //lANZAR EXCEPCIONES
-        try{
-            let data = await mainModel.create({name:req.body.name,user:req.body.user,password:req.body.password})
+        try {
+            let data = await mainModel.create({ name: req.body.name, user: req.body.user, password: req.body.password })
 
             res.status(201).json(data)
-        }catch(e){
-        
+        } catch (e) {
+
             console.log(e)
             next(e)
         }
     },
-    login: async function(req, res, next) {
+    login: async function (req, res, next) {
         //Consulto por usuario
-        let usuario = await mainModel.findOne({user:req.body.user})
-        if(usuario){
+        let usuario = await mainModel.findOne({ user: req.body.user })
+        if (usuario) {
             //Validar el password
-            if(bcrypt.compareSync(req.body.password,usuario.password)){
+            if (bcrypt.compareSync(req.body.password, usuario.password)) {
                 //Password valido , genero token
-                const token = jwt.sign({UserId:usuario._id},req.app.get('secretKey'),{expiresIn:'1h'})
-                res.status(201).json({token:token})
-            }else{
+                const token = jwt.sign({ UserId: usuario._id }, req.app.get('secretKey'), { expiresIn: '1h' })
+                res.status(201).json({ token: token })
+                    .json({
+                        name: usuario.name,
+                        user: usuario.user,
+                    })
+
+
+            } else {
                 //Password invalido
-                res.json({message:"Password incorrecto",data:null})
+                res.json({ message: "Password incorrecto", data: null })
             }
-        }else{
+        } else {
             //Arrojar error
-            res.json({message:"Usuario no existe",data:null})
+            res.json({ message: "Usuario no existe", data: null })
         }
 
-        
-        
-    }    
+
+
+    }
 }
