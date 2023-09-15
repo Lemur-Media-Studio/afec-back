059a414ebc99ab5459695c649a6ce075d4998d25
facumruiz <facundoruiz105@gmail.com>
2023-09-15 11:18:43 -0300

    fix 1 new script stripe.js

--- Back/routes/stripe.js
+++ Back/routes/stripe.js
@@ -1,5 +1,7 @@
-const stripe = require('stripe')('sk_test_51NpwRSDCxZVJxL3fgj7tsJ85VkpWy2DsDKp0rhMItM3EoJHyBryBlk6JKMaFnqoFvoiKmchq9pK5lgzFYCrRjubo00EflBfuoM');
+var express = require('express');
 var router = express.Router();
+const stripe = require('stripe')('sk_test_51NpwRSDCxZVJxL3fgj7tsJ85VkpWy2DsDKp0rhMItM3EoJHyBryBlk6JKMaFnqoFvoiKmchq9pK5lgzFYCrRjubo00EflBfuoM');
+
 
 router.post('/create-checkout-session', async (req, res) => {
     const session = await stripe.checkout.sessions.create({
