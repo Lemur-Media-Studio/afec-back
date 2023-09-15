8b960995239485ee2a1565594cc39148b3aca392
facumruiz <facundoruiz105@gmail.com>
2023-09-14 23:29:19 -0300

    test 1 email

--- Back/routes/stripe.js
+++ Back/routes/stripe.js
@@ -6,7 +6,7 @@ router.post("/payment", (req, res) => {
     {
       source: req.body.tokenId,
       amount: req.body.amount,
-      email:  req.body.receipt_email,
+      email:  req.body.email,
       currency: "eur"
  
 
