d757d532deeefab3b90b0d9d0959e932a14d2eaf
facumruiz <facundoruiz105@gmail.com>
2023-09-15 14:20:43 -0300

    fix 1 add new stripe script

--- Back/routes/stripe.js
+++ Back/routes/stripe.js
@@ -1,14 +1,12 @@
-const stripe = require('stripe')('sk_test_51NpwRSDCxZVJxL3fgj7tsJ85VkpWy2DsDKp0rhMItM3EoJHyBryBlk6JKMaFnqoFvoiKmchq9pK5lgzFYCrRjubo00EflBfuoM');
 var express = require('express');
 var router = express.Router();
-
-
-router.post('/check', async (req, res) => {
+const stripe = require('stripe')('sk_test_51NpwRSDCxZVJxL3fgj7tsJ85VkpWy2DsDKp0rhMItM3EoJHyBryBlk6JKMaFnqoFvoiKmchq9pK5lgzFYCrRjubo00EflBfuoM');
+router.post('/create-checkout-session', async (req, res) => {
     const session = await stripe.checkout.sessions.create({
       line_items: [
         {
           // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-          price: 'price_1NpwTeDCxZVJxL3fo1YjtMLB',
+          price: 'price_1NpwVkDCxZVJxL3fJNHGpzm2',
           quantity: 1,
         },
       ],
@@ -16,10 +14,9 @@ router.post('/check', async (req, res) => {
       success_url: `https://afec.onrender.com/success`,
       cancel_url: `https://afec.onrender.com/cancel`,
     });
-  
+
     res.redirect(303, session.url);
-    res.json(session)
+    console.log(session)
   });
 
-  
-module.exports = router;
+  module.exports = router;
